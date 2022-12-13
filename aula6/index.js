06e543b0cbfa455651fd4ffe0ebadc0698ad3a69
Luis Henrique <selesluishenrique@gmail.com>
2022-12-12 21:07:47 -0300

    Treinando variáveis constantes.

---
+++ aula6/index.js
@@ -0,0 +1,7 @@
+//const (Constantes)
+
+const primeiroN = 10;
+const segundoN = 60;
+const resultado = primeiroN * segundoN;
+const resultadoDuplicado = resultado * 2;
+console.log( typeof resultadoDuplicado);
